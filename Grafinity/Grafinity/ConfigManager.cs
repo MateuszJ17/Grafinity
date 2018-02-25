4ac2381d371764dd538b38e07bd0a13309254273
Unknown <ksiwinski96@gmail.com>
2018-02-25 23:00:43 +0100

    Auto stash before merge of "master" and "origin/master"
    ConfigManager, Basic Frontend

---
+++ Grafinity/Grafinity/ConfigManager.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Configuration;
+using System.Collections;
+using Newtonsoft.Json;
+
+namespace Grafinity
+{
+    public static class ConfigManager
+    {
+        static string config = File.ReadAllText(Directory.GetParent(Directory.GetCurrentDirectory()).Parent.FullName + "\\config.config");
+
+        public static void UpdateConfig()
+        {
+            Dictionary<string, string> values = new Dictionary<string, string>();
+            values.Add("key1", "value1");
+            values.Add("key2", "value2");
+            string json = JsonConvert.SerializeObject(values);
+            File.WriteAllText(Directory.GetParent(Directory.GetCurrentDirectory()).Parent.FullName + "\\config.config", json);
+        }
+
+        public static void ConfigContent()
+        {
+            Console.WriteLine(config);
+        }
+    }
+}
--- Grafinity/Grafinity/GrafinityWindow.cs
+++ Grafinity/Grafinity/GrafinityWindow.cs
@@ -2,10 +2,29 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace Grafinity
 {
-    class GrafinityWindow
+    class GrafinityWindow : Form
     {
+        public GrafinityWindow()
+        {
+            Button screenshot = new Button();
+            screenshot.Parent = this;
+            screenshot.Left = 20;
+            ComboBox comboBox1 = new ComboBox();
+            comboBox1.Parent = this;
+            comboBox1.Left = 100;
+            comboBox1.Items.Add("Normal Screenshot");
+        }
+
+        public void Initialize()
+        {
+            Button screenshot = new Button();
+            screenshot.Parent = this;
+            screenshot.Left = 100;
+        }
     }
 }
