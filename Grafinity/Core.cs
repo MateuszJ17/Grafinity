dc118c4d9fb4a84eef6f0a55bba3d7708dd6c5ca
Unknown <sneaky517@gmail.com>
2018-03-18 16:21:45 +0100

    Added wrapper

---
+++ Grafinity/Core.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace Grafinity
+{
+    static class Core
+    {
+        public static void Execute()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+
+            GrafinityWindow form = new GrafinityWindow();
+
+            Application.Run(form);
+        }
+
+        public static void Save()
+        {
+            ImageManager.SaveScreen(); // add bitmap as argument
+        }
+
+        public static void GetFiles()
+        {
+            ImageManager.GetFiles(); // get all graphical files in folder
+        }
+
+        public static void Capture()
+        {
+            ImageCapturer.Capture();
+        }
+
+        public static void BW()
+        {
+            ImageManipulator.BlackWhite();
+        }
+
+        public static void Negative()
+        {
+            ImageManipulator.Negative();
+        }
+
+        public static void Sepia()
+        {
+            ImageManipulator.Sepia();
+        }
+    }
+}
