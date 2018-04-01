9451faa961825a957158ecbaf8810bef1c11eba9
Unknown <sneaky517@gmail.com>
2018-04-01 16:23:03 +0200

    Core clean up

--- Grafinity/Core.cs
+++ Grafinity/Core.cs
@@ -15,36 +15,9 @@ namespace Grafinity
             GrafinityWindow form = new GrafinityWindow();
 
             Application.Run(form);
+
+            //TODO: add rest of funcionality
         }
 
-        public static void Save()
-        {
-            ImageManager.SaveScreen(); // add bitmap as argument
-        }
-
-        public static void GetFiles()
-        {
-            ImageManager.GetFiles(); // get all graphical files in folder
-        }
-
-        public static void Capture()
-        {
-            ImageCapturer.Capture();
-        }
-
-        public static void BW()
-        {
-            ImageManipulator.BlackWhite();
-        }
-
-        public static void Negative()
-        {
-            ImageManipulator.Negative();
-        }
-
-        public static void Sepia()
-        {
-            ImageManipulator.Sepia();
-        }
     }
 }
